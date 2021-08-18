6f8ed1676677c4505c3499c42b1ff1343126b9a2
CristiDablea <cristidablea@yahoo.com>
2021-08-18 21:31:29 +0300

    Metode Java

---
+++ src/MetodeJava.java
@@ -0,0 +1,10 @@
+public class MetodeJava {
+    public MetodeJava() {
+    }
+
+    public static void MetodeJava(String[] args) {
+        String nume = "Cristi";
+        boolean rezultat = nume.equals("Cristi");
+        System.out.println(rezultat);
+    }
+}
